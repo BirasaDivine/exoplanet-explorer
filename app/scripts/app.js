31572fdbf3d943cda4ae4eefe5d175f1459cb39b
Birasa Divine <b.divine@alustudent.com>
2025-04-27 21:07:13 +0200

    First-Thumb-Start

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,6 +64,14 @@ Instructions:
 
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+     getJSON('../data/earth-like-results.json')
+     .then(function(response){
+      console.log(response)
+      addSearchHeader(response.query)
+      createPlanetThumb(response.results[0])
+     })
+     .catch(function(error){
+      addSearchHeader('unknown')
+     })
   });
 })(document);
